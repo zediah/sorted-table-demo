96335d41914b031e2037d8863a6510af2a5d1a9e
Robert Harris <robert.harris@healthengine.com.au>
2018-05-30 14:24:58 +1000

    Was accidently updating table in another repo somehow...

--- src/table/table.js
+++ src/table/table.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import cn from 'classnames';
 import TableHeader from './tableHeader';
 import { partition } from '../utils/arrayUtil';
@@ -6,16 +6,18 @@ import { partition } from '../utils/arrayUtil';
 import './table.css';
 
 interface State {}
-interface Props {}
+interface Props {
+    dark?: boolean;
+}
 
 class Table extends React.Component<Props, State> {
     state: State = {};
 
     render() {
-        const { children, className, ...rest } = this.props;
+        const { children, className, dark, ...rest } = this.props;
         const [heading, content] = partition(React.Children.toArray(children), child => child.type === TableHeader);
         return (
-            <table {...rest} className={cn(className, 'table')}>
+            <table {...rest} className={cn(className, 'table', { dark })}>
                 {heading.length > 0 && heading[0]} 
                 <tbody>
                     {content}
